f79c834976556a74e4cb6c7bb1624ef2f504b32b
biklett10 <biklett10@live.com>
2009-10-26 12:24:57 -0700

    rake db:migrate && rake db:test:prepare

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091025224322) do
+ActiveRecord::Schema.define(:version => 20091026192313) do
+
+  create_table "genres", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "movies", :force => true do |t|
     t.datetime "created_at"
