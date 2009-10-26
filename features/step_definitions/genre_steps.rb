0acdf51a7866deb6a92a6dbe7979eb3517888a27
biklett10 <biklett10@live.com>
2009-10-26 12:35:20 -0700

    Added When-step to genre_steps.rb

--- features/step_definitions/genre_steps.rb
+++ features/step_definitions/genre_steps.rb
@@ -3,3 +3,7 @@
 Given /^a genre named Comedy$/ do
   @comedy = Genre.create!(:name => "Comedy")
 end
+
+When /^I create a movie Caddyshack in the Comedy genre$/ do
+  @caddyshack = Movie.create!(:name => "Caddyshack", :genre => "Comedy")
+end
