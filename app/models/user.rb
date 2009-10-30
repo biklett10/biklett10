8dfd3fcee1b489bba409a03648c092a2f8f1a9d9
biklett10 <biklett10@live.com>
2009-10-30 16:54:29 -0700

    step 2 matthooks/authlogic-activation-tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   acts_as_authentic
-end
+
+  # added to user.rb
+  attr_accessible :login, :email, :password, :password_confirmation, :openid_identifier
+
+  def active?
+    active
+  end
+
+end # class
+
