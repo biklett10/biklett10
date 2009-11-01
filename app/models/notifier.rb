fc6d2d8e2b12f38f9062cb2c1cf0b5956bdd31f9
biklett10 <biklett10@live.com>
2009-11-01 01:56:42 -0700

    Added activation_instructions() and activation_confirmation()

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,22 +1,22 @@
 class Notifier < ActionMailer::Base
-  
 
-  def activation_instructions(sent_at = Time.now)
-    subject    'Notifier#activation_instructions'
-    recipients ''
-    from       ''
-    sent_on    sent_at
-    
-    body       :greeting => 'Hi,'
+  # Syntax below derived from 
+  # http://github.com/matthooks/authlogic-activation-tutorial
+
+  def activation_instructions(user)
+    subject       "Activation Instructions"
+    from          "Binary Logic Notifier <noreply@binarylogic.com>"
+    recipients    user.email
+    sent_on       Time.now
+    body          :account_activation_url => register_url(user.perishable_token)
   end
 
-  def activation_confirmation(sent_at = Time.now)
-    subject    'Notifier#activation_confirmation'
-    recipients ''
-    from       ''
-    sent_on    sent_at
-    
-    body       :greeting => 'Hi,'
+  def activation_confirmation(user)
+    subject       "Activation Complete"
+    from          "Binary Logic Notifier <noreply@binarylogic.com>"
+    recipients    user.email
+    sent_on       Time.now
+    body          :root_url => root_url
   end
 
 end
