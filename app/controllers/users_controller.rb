9e176d008d6176e1aba8adbfd2577506d1e27987
biklett10 <biklett10@live.com>
2009-10-31 22:43:09 -0700

    step 6 github.com/matthooks/authlogic-activation-tutorial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(params[:user])
-    if @user.save
-      flash[:notice] = "Registration Almost Done."
-      redirect_to "/static/check_email"
+
+    # Saving without session maintenance to skip
+    # auto-login which can't happen here because
+    # the User has not yet been activated
+    if @user.save_without_session_maintenance
+      @user.deliver_activation_instructions!
+      flash[:notice] = "Your account has been created. Please check your e-mail for your account activation instructions!"
+      redirect_to root_url
     else
       render :action => :new
     end
-  end
+  end # def create
   
   def show
     @user = @current_user
