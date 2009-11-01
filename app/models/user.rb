113ae7198a99d52e50c8ce05678e8e5ed386d353
biklett10 <biklett10@live.com>
2009-10-31 21:55:06 -0700

    step 5 github.com/matthooks/authlogic-activation-tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,36 @@ class User < ActiveRecord::Base
     save
   end
 
+  # step 5
+  # http://github.com/matthooks/authlogic-activation-tutorial
+  # added to app/models/user.rb
+  def deliver_activation_instructions!
+    reset_perishable_token!
+    Notifier.deliver_activation_instructions(self)
+  end
+
+  def deliver_activation_confirmation!
+    reset_perishable_token!
+    Notifier.deliver_activation_confirmation(self)
+  end
+
+  # added to app/models/notifier.rb
+  def activation_instructions(user)
+    subject       "Activation Instructions"
+    from          "Binary Logic Notifier <noreply@binarylogic.com>"
+    recipients    user.email
+    sent_on       Time.now
+    body          :account_activation_url => register_url(user.perishable_token)
+  end
+
+  def activation_confirmation(user)
+    subject       "Activation Complete"
+    from          "Binary Logic Notifier <noreply@binarylogic.com>"
+    recipients    user.email
+    sent_on       Time.now
+    body          :root_url => root_url
+  end
+
+
 end # class
 
