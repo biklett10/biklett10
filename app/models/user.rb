15595c183f960cf07ab8b905b985a6eb8c55e3cb
biklett10 <biklett10@live.com>
2009-10-31 23:02:17 -0700

    Step 8 github.com/matthooks/authlogic-activation-tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
+  # For authlogic 2.0+
+  # step 8 http://github.com/matthooks/authlogic-activation-tutorial
+  acts_as_authentic do |c|
+    c.validates_length_of_password_field_options = {:on => :update, :minimum => 4, :if => :has_no_credentials?}
+    c.validates_length_of_password_confirmation_field_options = {:on => :update, :minimum => 4, :if => :has_no_credentials?}
+  end
+
 
   # added to user.rb
   attr_accessible :login, :email, :password, :password_confirmation, :openid_identifier
@@ -44,6 +50,23 @@ class User < ActiveRecord::Base
     recipients    user.email
     sent_on       Time.now
     body          :root_url => root_url
+  end
+
+  # step 8 http://github.com/matthooks/authlogic-activation-tutorial
+  # we need to make sure that either a password or openid gets set
+  # when the user activates his account
+  def has_no_credentials?
+    self.crypted_password.blank? && self.openid_identifier.blank?
+  end
+
+  # ...
+  # now let's define a couple of methods in the user model. The first
+  # will take care of setting any data that you want to happen at signup
+  # (aka before activation)
+  def signup!(params)
+    self.login = params[:user][:login]
+    self.email = params[:user][:email]
+    save_without_session_maintenance
   end
 
 
