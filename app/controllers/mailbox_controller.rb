77fffad646d0ae5678eca3ce0867aeb2b185b351
biklett10 <biklett10@live.com>
2009-11-05 02:40:48 -0800

    Copied over controllers and views from mssgr

--- app/controllers/mailbox_controller.rb
+++ app/controllers/mailbox_controller.rb
@@ -1,5 +1,21 @@
 class MailboxController < ApplicationController
-  def show
+  def index
+    redirect_to new_session_path and return unless logged_in?
+    @folder = current_user.inbox
+    show
+    render :action => "show"
   end
 
+  def show
+    @folder ||= current_user.folders.find(params[:id])
+    @messages = @folder.messages.paginate_not_deleted :all, :per_page => 10, :page => params[:page],
+          :include => :message, :order => "messages.created_at DESC"
+  end
+  
+  def trash
+    @folder = Struct.new(:name, :user_id).new("Trash", current_user.id)
+    @messages = current_user.received_messages.paginate_deleted :all, :per_page => 10, :page => params[:page],
+          :include => :message, :order => "messages.created_at DESC"
+    render :action => "show"
+  end
 end
