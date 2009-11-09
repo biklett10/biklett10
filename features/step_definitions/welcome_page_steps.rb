62ee20f20d7895b8951fa9c87c11aacfe7b3a15a
biklett10 <biklett10@live.com>
2009-11-09 04:44:33 -0800

    Worked around problem with have_selector in welcome_page_steps.rb

--- features/step_definitions/welcome_page_steps.rb
+++ features/step_definitions/welcome_page_steps.rb
@@ -1,38 +1,42 @@
 # welcome_page_steps.rb
-
+ 
 Given /^the server is up$/ do
   # its up
 end
-
+ 
 Given /^I am not logged in$/ do
   # I am not supposed to be logged in.
   # Raise an error if I am logged in.
   raise("I am logged in") unless defined?(current_user).nil?
 end
-
+ 
 # When I go to the homepage
-
+ 
 Then /^I should see a page with 2 columns$/ do
-  doc = Hpricot(response.body)
-  (doc/"body/table.main_table/tr.main_tr/td.lhs/../td").size.should == 2
+  @doc = Hpricot(response.body)
+  @nok = Nokogiri::HTML(response.body)
+  (@doc/"body/table.main_table/tbody/tr.main_tr/td.lhs/../td").size.should == 2
 end
-
+ 
 Then /^the LHS column should be relatively narrow$/ do
   # I dont know how to compare the width of LHS with RHS.
   # So just look at it.
 end
-
+ 
 Then /^the RHS column should be much more prominent$/ do
   # I dont know how to compare the width of LHS with RHS
   # So just look at it.
 end
-
+ 
 Then /^the LHS column should have the links: login, register$/ do
-  response.should_not have_selector("a", :href => "/" , :content => "About") 
-  response.should have_selector("a", :href => "/login" , :content => "Login") 
-  response.should have_selector("a", :href => "/register" , :content => "Register") 
+  (@doc/"a[@href=/]").size.should == 0
+  (@doc/"a[@href=/login]").html.should == "Login"
+  (@doc/"a[@href=/register]").html.should == "Register"
 end
-
+ 
 Then /^the RHS should only contain the word Welcome$/ do
-  response.should have_selector("html/body/table.main_table/tr.main_tr/td.rhs/h1", :content => "Welcome") 
+  # !((@doc/"body/table.main_table tr.main_tr/td.rhs/h1").html =~ /Welcome/).nil?
+  debugger
+  ((@doc/"body/table.main_table tr.main_tr/td.rhs/h1").html.should =~ /Welcome/)
 end
+ 
