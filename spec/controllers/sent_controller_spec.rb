ad4b53eba0816cf0dc9b1085d3facf6025cafe4e
biklett10 <biklett10@live.com>
2009-11-11 14:48:31 -0800

    I cant figure out how to set current_user inside the controller

--- spec/controllers/sent_controller_spec.rb
+++ spec/controllers/sent_controller_spec.rb
@@ -2,28 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe SentController do
 
-  #Delete these examples and add some real ones
+  # Bikle 2009-11-11
   it "should use SentController" do
     controller.should be_an_instance_of(SentController)
   end
-
-
-  describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'show'" do
-    it "should be successful" do
-      get 'show'
-      response.should be_success
-    end
-  end
-
+  # new corresponds to "Compose" in the view.
   describe "GET 'new'" do
     it "should be successful" do
+      debugger
       get 'new'
       response.should be_success
     end
