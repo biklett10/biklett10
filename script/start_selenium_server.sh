ef009d0f9b23f87e59a200fd14afe6894adc567f
biklett10 <biklett10@live.com>
2009-11-11 22:14:04 -0800

    Added appropriate mongrel call to selenium start script

--- script/start_selenium_server.sh
+++ script/start_selenium_server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # script/start_selenium_server.sh
 # Run this in a separate window from RAILS_ROOT
-export RAILS_ENV=test
+export RAILS_ENV=selenium
 export ss_jar=vendor/gems/webrat-0.4.4/vendor/selenium-server.jar
  
-/usr/bin/java -jar ${ss_jar} -multiwindow
- 
+/usr/bin/java -jar ${ss_jar} -multiwindow > /dev/null 2>&1 &
+# Start mongrel with debugger and on port 3001
+script/server --debugger -p 3001
