523b4a4d47048e8608119de360a50a67623b51fd
biklett10 <biklett10@live.com>
2009-11-26 01:38:41 -0800

    Fixed bug in StaticController related to repel_em filter

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,9 @@
 class StaticController < ApplicationController
-  before_filter :repel_em, :except => :about
+  before_filter :repel_em
+  def repel_em
+    super if %w[messages].include? self.action_name
+  end
+
   def about
   end
   def check_email
