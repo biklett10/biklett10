c7e113658c57b2305adca468e78e6a8d86d00c81
biklett10 <biklett10@live.com>
2009-12-06 23:35:42 -0800

    Enhanced user_sessions_controller.rb to deal with session state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,21 @@ class ApplicationController < ActionController::Base
   include AuthlogicBundle::Localization
   include SslRequirement
 
+  # bikle 2009-12-06
+  before_filter :find_the_user
+  def find_the_user
+    case 
+    when session[:user_credentials_id].nil?
+      @the_user= "not_logged_in"
+    when User.find(session[:user_credentials_id]).nil?
+      @the_user= "not_logged_in"
+    else
+      @the_user= User.find(session[:user_credentials_id]).email
+    end
+    @the_user
+  end
+  # bikle 2009-12-06
+
   def ssl_required?
     return ENV['SSL'] == 'on' ? true : false if defined? ENV['SSL']
     return false if local_request?
---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,39 @@
+class UserSessionsController < ApplicationController
+  ssl_required :new, :create, :destroy
+  before_filter :require_no_user, :only => [:new, :create]
+  # before_filter :require_user, :only => :destroy
+
+  # GET /login
+  def new
+    @user_session = UserSession.new
+  end
+
+  # POST /user_session
+  def create
+    @user_session = UserSession.new(params[:user_session])
+
+    @user_session.save do |result|
+      if result
+        flash[:success] = t('user_sessions.flashs.success.create')
+        session[:language] = nil
+        session[:return_to] = nil if session[:return_to] == "/logout"
+        redirect_back_or_default account_url
+      else
+        render :action => :new
+      end
+    end
+  end
+
+  # DELETE /logout
+  def destroy
+    current_user_session.destroy unless current_user_session.nil?
+    flash[:success] = t('user_sessions.flashs.success.destroy')
+    session[:language] = nil
+    session[:return_to] = nil
+    # bikle 2009-12-06
+    session[:user_credentials]= nil
+    session[:user_credentials_id]= nil
+    # bikle 2009-12-06
+    redirect_back_or_default login_url
+  end
+end
