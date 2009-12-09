d93ecb706075ebd050df0c93a5b717f6f00fe032
biklett10 <biklett10@live.com>
2009-12-08 22:46:22 -0800

    Added syntax to support msg

--- vendor/plugins/authlogic_bundle/app/models/user.rb
+++ vendor/plugins/authlogic_bundle/app/models/user.rb
@@ -4,4 +4,20 @@ class User < ActiveRecord::Base
   acts_as_authentic
 
   named_scope :active, :conditions => ['state = ?', 'active']
+  # bikle 2009-1208
+  # http://www.novawave.net/public/rails_messaging_tutorial.html
+  has_many :sent_messages, :class_name => "Message", :foreign_key => "author_id"
+  has_many :received_messages, :class_name => "MessageCopy", :foreign_key => "recipient_id"
+  has_many :folders
+  before_create :build_inbox
+  
+  def inbox
+    folders.find_by_name("Inbox")
+  end
+  
+  def build_inbox
+    folders.build(:name => "Inbox")
+  end
+  # http://www.novawave.net/public/rails_messaging_tutorial.html
+  # bikle 2009-1208
 end
